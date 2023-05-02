6c165aa7ad4ab77d83efa3616c6c1c64514892ad
Angel Orozco <angeldorozcon@gmail.com>
2023-05-01 23:05:11 -0300

    Completed Learning 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6, 7);
+
+        Console.WriteLine($"{f1.GetTop()} {f1.GetBottom()}");
+        Console.WriteLine($"{f2.GetTop()} {f2.GetBottom()}");
+        Console.WriteLine($"{f3.GetTop()} {f3.GetBottom()}");
+
+        f1.SetTop(4);
+        f1.SetBottom(3);
+
+        Console.WriteLine($"{f1.GetTop()} {f1.GetBottom()}");
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetFractionValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetFractionValue());
+
     }
 }
